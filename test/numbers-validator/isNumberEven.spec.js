1e79bacd69a16d3d21f5162215132c4118094cca
Kira Vlasova <vlsv-kira@ya.ru>
2023-10-30 21:49:58 +0100

    added more tests. added c8rc code coverage analyzer

--- test/numbers-validator/isNumberEven.spec.js
+++ test/numbers-validator/isNumberEven.spec.js
@@ -2,7 +2,7 @@ import {NumbersValidator} from '../../app/numbers-validator.js'
 import {expect} from 'chai'
 
 // test suite
-describe('is number even positive test', () => {
+describe('is number even tests', () => {
 
   let validator
 
@@ -11,10 +11,26 @@ describe('is number even positive test', () => {
     validator = new NumbersValidator()
   })
 
+  afterEach(() => {
+    validator = null
+  })
+
   // individual test
   it('should return true when even number is provided', () => {
     const validationResults = validator.isNumberEven(4);
     // chai feature
     expect(validationResults).to.be.equal(true)
+  });
+
+  it('should return false when odd number is provided', () => {
+    const validationResults = validator.isNumberEven(5);
+    // chai feature
+    expect(validationResults).to.be.equal(false)
+  });
+
+  it('should throw an error when string is provided', () => {
+    expect(() => {
+      validator.isNumberEven('4')
+    }).to.throw(`[4] is not of type "Number" it is of type "string"`)
   });
 })
